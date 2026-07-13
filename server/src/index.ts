e215956d9f71e8eeb51bdf7032911c3af4a76036
MarcosPimienta <fenix3819@gmail.com>
2026-07-13 16:45:52 -0500

    Use custom explicit CORS middleware in Express index

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,7 +5,16 @@ import inviteRouter from './routes/invite';
 const app = express();
 const PORT = process.env.PORT || 5001;
 
-app.use(cors());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Role');
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(204);
+    return;
+  }
+  next();
+});
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
